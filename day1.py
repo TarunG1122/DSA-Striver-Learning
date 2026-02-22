19b77f563ac1ee12850923aba2340fcde0b3f4f8
TarunG1122 <infotarun2000@gmail.com>
2026-02-22 23:01:09 +0530

    Updated day1.py logic

--- day1.py
+++ day1.py
@@ -1,1 +1,23 @@
-print("hello")
+# learning basic recursion
+
+def learning_recursion(n):
+
+    if n == 0:
+        return
+
+    learning_recursion(n-1)
+    print(n)
+
+learning_recursion(5)
+
+
+
+def learning_recursion(n):
+
+    if n == 0:
+        return
+    print(n)
+    learning_recursion(n-1)
+
+
+learning_recursion(5)
