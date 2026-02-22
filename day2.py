c3ab82dad46ad5d9cbb7a50507f608c144d531fc
TarunG1122 <infotarun2000@gmail.com>
2026-02-22 23:37:50 +0530

    learning basic recursion day2

--- day2.py
+++ day2.py
@@ -1,11 +1,49 @@
-# printing name n times using recusion
+# Strive link: https://www.youtube.com/watch?v=un6PLygfXrA&list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz&index=10
 
-def priting_name(n):
+# # printing name n times using recusion
+#
+# def priting_name(n):
+#
+#     if n == 0:
+#         return
+#
+#     print("tarun")
+#     priting_name(n-1)
+#
+# priting_name(5)
+#
 
-    if n == 0:
-        return
 
-    print("tarun")
-    priting_name(n-1)
 
-priting_name(5)
+####################
+
+## print linearly from 1 to N
+
+#
+# def print_linearly(n):
+#
+#     if n == 0:
+#         return
+#
+#     print_linearly(n-1)
+#     print(n)
+#
+# print_linearly(5)
+#
+
+
+#
+# ## print linearly from N to 1
+#
+#
+# def print_linearly(n):
+#
+#     if n == 0:
+#         return
+#     print(n)
+#     print_linearly(n-1)
+#
+#
+# print_linearly(5)
+
+###########################################
